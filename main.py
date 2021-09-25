059a8e5be75d42af5f0a17db4c66fcafea5f2594
vanchenkin <vanchenkin@yandex.ru>
2021-09-25 12:09:36 +0300

    command commi

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ import discord
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+
+
+import src.commands
 
 client.run(os.environ.get('DISCORD_TOKEN'))
