b6b0aaa8899d196e3a12ff2626af93f1e07b0c4c
MilkyCode13 <shmayhel.andrey@gmail.com>
2021-09-25 12:06:05 +0300

    Fixed Docker and misc

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
 
 client.run(os.environ.get('DISCORD_TOKEN'))
