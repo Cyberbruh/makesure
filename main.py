570fde5a811176b5c8b1b931391a9a5b13d9c92a
Kirill <shabad.k@yandex.ru>
2021-09-25 14:27:39 +0300

    Added callings

--- main.py
+++ main.py
@@ -8,14 +8,32 @@ import src.mongo
 
 load_dotenv()
 
-db = MongoClient('mongodb://localhost:27017/').makesure
-client = discord.Client()
+dlgs = []
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
 @bot.command()
 async def start(ctx):
-    author = ctx.message.author
-    await ctx.send(f'Hello, {author.mention}!')
+    await ctx.author.send('Введите тег пользователя, с которым вы хотите начать спор?')
+    print(ctx.author.id)
+    dlgs.append(ctx.author)
+    members = ctx.guild.members
 
-bot.run(os.getenv('DISCORD_TOKEN'))
+    def check(m):
+        return m.author == ctx.author
+    msg = await bot.wait_for("message", check=check)
+    for m in members:
+        if (m.name + '#' + m.discriminator) == msg.content:
+            await m.send('Бросаю перчатку!')
+
+#bot.remove_command(startt)
+#bot.add_command(startt)
+
+
+
+bot.run(os.environ.get('DISCORD_TOKEN'))
