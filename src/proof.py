623f5f0f9c1f2cfa6b1298cfcc041e9cd0a2dc08
vanchenkin <vanchenkin@yandex.ru>
2021-09-25 14:52:54 +0300

    ORM classes made

--- src/proof.py
+++ src/proof.py
@@ -1,11 +1,10 @@
-import datetime
+from datetime import *
+from mongoengine import *
 
 from .dispute import Dispute
 
-class Proof:
-    def __init__(self, db, dispute: Dispute, user_id: int, description: int) -> None:
-        self.dispute = dispute.id
-        self.user_id = user_id
-        self.description = description
-        self.date = datetime.datetime.now()
-        self.id = db.proofs.insert_one(self.__dict__).inserted_id
+class Proof(Document):
+    disput = ReferenceField(Dispute)
+    user_id = IntField(required=True)
+    description = StringField()
+    date = DateTimeField(default=datetime.utcnow)
--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from src.proof import *
 load_dotenv()
 connect(host="mongodb://127.0.0.1:27017/makesure")
 
-d1 = Dispute(user1_id=21, user2_id=123,description="23", amount=12)
+d1 = Dispute(user1_id=21, user2_id=123, description="23", amount=12)
 d1.save()
 #prof = Proof(db, d1, 123, "aDAWD")
 #dep = Deposit(db, 213, d1, 12)
