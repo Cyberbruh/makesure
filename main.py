dcbab93a6914790856a94f20e2640f0ea62e909e
Kirill <shabad.k@yandex.ru>
2021-09-25 16:06:18 +0300

    Added full dialog

--- main.py
+++ main.py
@@ -6,31 +6,38 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-dlgs = []
+dialogs = []
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
+
+
+async def dialog(usr, members):
+
+    def check(m):
+        return m.author == usr
+    opponent_tag = await bot.wait_for("message", check=check)
+    opponent = None
+    await usr.send('Введите сумму вашего взноса')
+    fee = await bot.wait_for('message', check=check)
+    await usr.send('Опишите суть вашего спора')
+    desc = await bot.wait_for('message', check=check)
+    for m in members:
+        if (m.name + '#' + m.discriminator) == opponent_tag.content:
+            opponent = m
+    await opponent.send(f'{usr.name} вызывает вас на спор. Сумма ставки {fee.content} тугриков. {desc.content}')
+
 
 @bot.command()
 async def start(ctx):
     await ctx.author.send('Введите тег пользователя, с которым вы хотите начать спор?')
-    print(ctx.author.id)
-    dlgs.append(ctx.author)
     members = ctx.guild.members
+    await dialog(ctx.author, members)
 
-    def check(m):
-        return m.author == ctx.author
-    msg = await bot.wait_for("message", check=check)
-    for m in members:
-        if (m.name + '#' + m.discriminator) == msg.content:
-            await m.send('Бросаю перчатку!')
-
-#bot.remove_command(startt)
-#bot.add_command(startt)
 
 
 
